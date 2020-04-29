5369072aca2e97710e06970d0a59de260bae2f34
Michal Niewrzal <michal@storj.io>
2020-04-29 13:16:02 +0200

    go.mod: bump common and storj versions
    Change-Id: If40eaf89e646acf9a886852ee864e37a80fbdbb6

--- scripts/check-libuplink-size.sh
+++ scripts/check-libuplink-size.sh
@@ -7,11 +7,11 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 go build -v -o $SCRIPTDIR/../.build/build.out $SCRIPTDIR/../examples/walkthrough/main.go
 
 BUILD_SIZE=$(wc -c < $SCRIPTDIR/../.build/build.out)
-CURRENT_SIZE=15359586 # for commit hash: fc4ab8256b07d89ebb08095088aa37864b9766ca
+CURRENT_SIZE=14500000 
 
 if [ $BUILD_SIZE -gt $CURRENT_SIZE ]; then
     echo "Libuplink size is too big, was $CURRENT_SIZE but now it is $BUILD_SIZE"
     exit 1
 fi
 
-echo "Libuplink size did not increase and it is $BUILD_SIZE"
+echo "Libuplink size did not increase and it is $BUILD_SIZE (limit: $CURRENT_SIZE)"
--- testsuite/go.mod
+++ testsuite/go.mod
@@ -7,7 +7,7 @@ replace storj.io/uplink => ../
 require (
 	github.com/stretchr/testify v1.5.1
 	go.uber.org/zap v1.14.1
-	storj.io/common v0.0.0-20200424175742-65ac59022f4f
-	storj.io/storj v0.12.1-0.20200427175457-9b4a3f8fcc6c
-	storj.io/uplink v1.0.4
+	storj.io/common v0.0.0-20200429074521-4ba140e4b747
+	storj.io/storj v0.12.1-0.20200429120635-70fe974a95c1
+	storj.io/uplink v1.0.5-0.20200428120514-235330485c11
 )
