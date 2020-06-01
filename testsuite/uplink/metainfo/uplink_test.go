e7bcc08962cd67737c09e0f5c1163fe49758a022
Michal Niewrzal <michal@storj.io>
2020-06-01 09:53:28 +0200

    testsuite/uplink/metainfo: enable TestMultisegmentUploadWithLastInline test
    We sill have issue in metainfo validation but its low priority.
    
    Change-Id: I60bfe7d76b747b5441cad65ae0858b22af7696ab

--- testsuite/uplink/metainfo/uplink_test.go
+++ testsuite/uplink/metainfo/uplink_test.go
@@ -15,19 +15,17 @@ import (
 )
 
 func TestMultisegmentUploadWithLastInline(t *testing.T) {
-	t.Skip("Needs a fix in https://github.com/storj/storj/blob/7e0e74c65cfc498acb75e957be61f1d2641da9a9/satellite/metainfo/validation.go#L339")
-
 	// this is special case were uploaded object has 3 segments (2 remote + 1 inline)
 	testplanet.Run(t, testplanet.Config{
 		SatelliteCount: 1, StorageNodeCount: 4, UplinkCount: 1,
 		Reconfigure: testplanet.Reconfigure{
-			Satellite: testplanet.MaxSegmentSize(10 * memory.KiB),
+			Satellite: testplanet.MaxSegmentSize(20 * memory.KiB),
 		},
 	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
 		satellite := planet.Satellites[0]
 		uplink := planet.Uplinks[0]
 
-		expectedData := testrand.Bytes(50 * memory.KiB)
+		expectedData := testrand.Bytes(40 * memory.KiB)
 
 		err := planet.Uplinks[0].Upload(ctx, planet.Satellites[0], "testbucket", "test/path", expectedData)
 		require.NoError(t, err)
