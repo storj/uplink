bd3283932aa3a100a1a9cd2e453f4367275d2637
Michał Niewrzał <michal@storj.io>
2021-08-24 11:41:00 +0200

    private/metaclient: add support for FinishMoveObject
    This change adds only support for FinishMoveObject in
    metaclient. Its one of two methods needed to handle server side move.
    
    Change-Id: Ia740985a6270c261d233c946cb5dcb6a18bfb670

--- private/metaclient/client_move.go
+++ private/metaclient/client_move.go
@@ -86,3 +86,50 @@ func (client *Client) BeginMoveObject(ctx context.Context, params BeginMoveObjec
 
 	return newBeginMoveObjectResponse(response), nil
 }
+
+// FinishMoveObjectParams parameters for FinishMoveObject method.
+type FinishMoveObjectParams struct {
+	StreamID                     storj.StreamID
+	NewEncryptedObjectKey        []byte
+	NewEncryptedMetadataKeyNonce storj.Nonce
+	NewEncryptedMetadataKey      []byte
+	NewSegmentKeys               []EncryptedKeyAndNonce
+}
+
+func (params *FinishMoveObjectParams) toRequest(header *pb.RequestHeader) *pb.ObjectFinishMoveRequest {
+	keys := make([]*pb.EncryptedKeyAndNonce, len(params.NewSegmentKeys))
+	for i, keyAndNonce := range params.NewSegmentKeys {
+		keys[i] = &pb.EncryptedKeyAndNonce{
+			EncryptedKeyNonce: keyAndNonce.EncryptedKeyNonce,
+			EncryptedKey:      keyAndNonce.EncryptedKey,
+		}
+	}
+	return &pb.ObjectFinishMoveRequest{
+		Header:                       header,
+		StreamId:                     params.StreamID,
+		NewEncryptedObjectKey:        params.NewEncryptedObjectKey,
+		NewEncryptedMetadataKeyNonce: params.NewEncryptedMetadataKeyNonce,
+		NewEncryptedMetadataKey:      params.NewEncryptedMetadataKey,
+		NewSegmentKeys:               keys,
+	}
+}
+
+// BatchItem returns single item for batch request.
+func (params *FinishMoveObjectParams) BatchItem() *pb.BatchRequestItem {
+	return &pb.BatchRequestItem{
+		Request: &pb.BatchRequestItem_ObjectFinishMove{
+			ObjectFinishMove: params.toRequest(nil),
+		},
+	}
+}
+
+// FinishMoveObject finishes process of moving object from one key to another.
+func (client *Client) FinishMoveObject(ctx context.Context, params FinishMoveObjectParams) (err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	err = WithRetry(ctx, func(ctx context.Context) error {
+		_, err = client.client.FinishMoveObject(ctx, params.toRequest(client.header()))
+		return err
+	})
+	return Error.Wrap(err)
+}
