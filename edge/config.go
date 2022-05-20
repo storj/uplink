202ea09a93c5fc7322565f375cfca3a9c6afb45c
Ivan Fraixedes <ivan@fraixed.es>
2022-05-20 15:50:45 +0200

    edge: Rewrite doc comments config fields
    Rewrite the doc comments of the Config fields to follow the usual Go
    conventions.
    
    Change-Id: I573a715f0b3aae1434dfcf41f6775efc42706925

--- edge/config.go
+++ edge/config.go
@@ -12,20 +12,19 @@ import (
 
 // Config contains configuration on how to access edge services.
 type Config struct {
-	// AuthServiceAddress: set a fixed DRPC server including port.
-	// valid is auth.[eu|ap|us]1.storjshare.io:443
-	// or a thirdparty-hosted alternative.
+	// AuthServiceAddress sets a fixed DRPC server including port.
+	// Valid is auth.[eu|ap|us]1.storjshare.io:443
+	// or a third party hosted alternative.
 	//
 	// Theoretically we can select the address based on the region
 	// of the satellite in the access grant but there is no consensus
 	// on the choice of mechanism to achieve that.
 	AuthServiceAddress string
 
-	// Root certificate(s) or chain(s) against which Uplink checks
-	// the auth service.
+	// CertificatePEM contains the root certificate(s) or chain(s) against which
+	// Uplink checks the auth service.
 	// In PEM format.
-	// Intended to test against a self-hosted auth service
-	// or to improve security
+	// Intended to test against a self-hosted auth service or to improve security.
 	CertificatePEM []byte
 }
 
