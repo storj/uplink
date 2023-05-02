1fc65b0def5515c93b00d200212103b5cc7dbb3b
dlamarmorgan <damein@storj.io>
2023-05-02 16:05:58 -0700

    testsuite/scripts: Add test scripts for rclone using storj-up
    Change-Id: I341af526fa38a7e3f575c367a5cd3d73c1f0de4d

--- testsuite/scripts/rclone.sh
+++ testsuite/scripts/rclone.sh
@@ -3,15 +3,42 @@ set -ueo pipefail
 set +x
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $SCRIPTDIR
 
 # setup tmpdir for testfiles and cleanup
 TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
 cleanup(){
-	rm -rf "$TMP"
+    if [ -f "$TMP"/docker-compose.yaml ]
+    then
+      docker compose -f "$TMP"/docker-compose.yaml down
+    fi
+  rm -rf "$TMP"
 }
 trap cleanup EXIT
 
-cd $TMP; git clone https://github.com/rclone/rclone
+STORJ_VERSION=$(go list -modfile "../go.mod" -m -f "{{.Version}}" storj.io/storj)
+COMMIT_HASH="${STORJ_VERSION##*-}"
+
+go install storj.io/storj/cmd/satellite@"$STORJ_VERSION"
+go install storj.io/storj/cmd/storagenode@"$STORJ_VERSION"
+go install storj.io/storj-up@main
+
+echo "Used storj version: $STORJ_VERSION"
+
+cd "$TMP"
+storj-up init minimal,db > /dev/null 2>&1
+storj-up build -s remote github satellite-api -c "$COMMIT_HASH"
+storj-up build -s remote github storagenode -c "$COMMIT_HASH"
+storj-up port remove cockroach 26257
+storj-up port add cockroach 26257 -e 26666
+
+# start the services
+docker compose build
+docker compose up -d
+storj-up health -d 60 -p 26666
+eval $(storj-up credentials -e)
+
+git clone https://github.com/rclone/rclone
 RCLONE=$TMP/rclone
 
 pushd $RCLONE
@@ -25,7 +52,7 @@ pushd $RCLONE
     go build -mod=mod ./fstest/test_all
     go build -mod=mod
 
-    ./rclone config create TestTardigrade tardigrade access_grant $GATEWAY_0_ACCESS
+    ./rclone config create TestTardigrade tardigrade access_grant $UPLINK_ACCESS
 
     ./test_all -backends tardigrade -output $SCRIPTDIR/../../.build/rclone-integration-tests
 popd
