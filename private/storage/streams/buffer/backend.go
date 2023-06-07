72bcffbeac33146027b0f70fc6fcd703fdf8bfb0
Jeff Wendling <leterip@gmail.com>
2023-06-07 13:47:49 -0400

    private/storage/streams/buffer: fix memory leak
    the upload code path is careful to close the underlying
    buffers and backends when it is done reading and writing
    them, but the memory backend didn't actually free the
    resources when closed. combined with the fact that we keep
    a reference to all of the segments for the duration of the
    upload, this meant that we were storing the entire upload
    data in memory until the upload was complete. oops.
    
    Change-Id: I3334a06ddd0346b09fc3bcd8870ccbb83b6ea108

--- private/storage/streams/buffer/backend.go
+++ private/storage/streams/buffer/backend.go
@@ -5,7 +5,7 @@ package buffer
 
 import (
 	"io"
-	"sync/atomic"
+	"sync"
 )
 
 // Backend is a backing store of bytes for a Backend.
@@ -18,42 +18,52 @@ type Backend interface {
 // NewMemoryBackend returns a MemoryBackend with the provided initial
 // capacity. It implements the Backend interface.
 func NewMemoryBackend(cap int) *MemoryBackend {
-	buf := make([]byte, 0, cap)
-	mbuf := new(MemoryBackend)
-	mbuf.buf.Store(buf)
-	return mbuf
+	return &MemoryBackend{
+		buf: make([]byte, 0, cap),
+	}
 }
 
 // MemoryBackend implements the Backend interface backed by a slice.
 type MemoryBackend struct {
-	buf atomic.Value // []byte
-}
-
-// Reset clears the length and allows the capacity to be reused.
-func (u *MemoryBackend) Reset() {
-	buf, _ := u.buf.Load().([]byte)
-	u.buf.Store(buf[:0])
+	mu     sync.Mutex
+	buf    []byte
+	closed bool
 }
 
 // Write appends the data to the buffer.
 func (u *MemoryBackend) Write(p []byte) (n int, err error) {
-	buf, _ := u.buf.Load().([]byte)
-	buf = append(buf, p...)
-	u.buf.Store(buf)
+	u.mu.Lock()
+	defer u.mu.Unlock()
+
+	if u.closed {
+		return 0, io.ErrClosedPipe
+	}
+	u.buf = append(u.buf, p...)
+
 	return len(p), nil
 }
 
 // ReadAt reads into the provided buffer p starting at off.
 func (u *MemoryBackend) ReadAt(p []byte, off int64) (n int, err error) {
-	buf, _ := u.buf.Load().([]byte)
-	if off >= int64(len(buf)) {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+
+	if u.closed {
+		return 0, io.ErrClosedPipe
+	} else if off >= int64(len(u.buf)) {
 		return 0, io.EOF
 	}
-	n = copy(p, buf[off:])
+	n = copy(p, u.buf[off:])
+
 	return n, nil
 }
 
-// Close is a no-op for a memory backend.
+// Close releases memory and causes future calls to ReadAt and Write to fail.
 func (u *MemoryBackend) Close() error {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+
+	u.buf = nil
+	u.closed = true
 	return nil
 }
