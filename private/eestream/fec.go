8966f4365d456c4637b71827390a606f60b7f3ad
Egon Elbre <egonelbre@gmail.com>
2023-10-31 14:57:47 +0200

    private/eestream: add infectious aliases
    We need to ensure that storj/storj doesn't directly depend on infectious
    library, that way we can change it to use our own package.
    
    Change-Id: I17da24f3995be0e3db33ccdc9556359c363055d2

---
+++ private/eestream/fec.go
@@ -0,0 +1,17 @@
+// Copyright (C) 2023 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package eestream
+
+import "github.com/vivint/infectious"
+
+// A Share represents a piece of the FEC-encoded data.
+type Share = infectious.Share
+
+// NewFEC creates a *FEC using k required pieces and n total pieces.
+// Encoding data with this *FEC will generate n pieces, and decoding
+// data requires k uncorrupted pieces. During decode, when more than
+// k pieces exist, corrupted data can be detected and recovered from.
+func NewFEC(k, n int) (*infectious.FEC, error) {
+	return infectious.NewFEC(k, n)
+}
