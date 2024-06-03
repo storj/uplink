d7ed28ee45a50f6253c296723f82206367a8e036
Artur M. Wolff <artur@storj.io>
2024-06-03 20:31:37 +0200

    testsuite: add BenchmarkProject_OpenProject
    goos: linux
    goarch: amd64
    pkg: storj.io/uplink/testsuite
    cpu: AMD Ryzen 5 1600 Six-Core Processor
                        │      O       │                  N                  │
                        │    sec/op    │   sec/op     vs base                │
    Project_OpenProject   585.03µ ± 5%   20.29µ ± 1%  -96.53% (p=0.000 n=10)
    
                        │       O       │                  N                   │
                        │     B/op      │     B/op      vs base                │
    Project_OpenProject   26.634Ki ± 0%   3.377Ki ± 0%  -87.32% (p=0.000 n=10)
    
                        │      O      │                 N                  │
                        │  allocs/op  │ allocs/op   vs base                │
    Project_OpenProject   449.00 ± 0%   41.00 ± 0%  -90.87% (p=0.000 n=10)
    
    Change-Id: Ief40a800add4c80d41460afca6dcd65d3dd330fa

--- testsuite/project_test.go
+++ testsuite/project_test.go
@@ -4,7 +4,9 @@
 package testsuite_test
 
 import (
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -12,6 +14,8 @@ import (
 	"storj.io/storj/private/testplanet"
 	"storj.io/uplink"
 )
+
+const testAccess = "1HYC799Xw6VsvPF1DnbQieSqTNtfousmMUb34uef5ZJRkW3NZSRLYFmb2SvRh7GJD4BF7T3ZmKeGRJF1ZsCrJxWwKz1EsrbemiXRywbHZ6AkYfvgpYbeFbhVeW1vndMYNe54GZYEMjZAihFkg27tQFCrRSYm2powghLTm4RqxucgP9KpgHzi6zmAWNWMx59TKCPRCY7o2r2H"
 
 func TestProject_OpenProjectDialFail(t *testing.T) {
 	testplanet.Run(t, testplanet.Config{
@@ -45,4 +49,43 @@ func TestProject_OpenProjectMalformedUserAgent(t *testing.T) {
 	_, err := config.OpenProject(ctx, &uplink.Access{})
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "user agent")
+}
+
+func BenchmarkProject_OpenProject(b *testing.B) {
+	b.ReportAllocs()
+
+	ctx := testcontext.New(b)
+	defer ctx.Cleanup()
+
+	clientCertPEM, clientKeyPEM := loadPEMs(b, "testdata/testidentity/identity.cert", "testdata/testidentity/identity.key")
+
+	config := uplink.Config{
+		DialTimeout: 10 * time.Second,
+		ChainPEM:    clientCertPEM,
+		KeyPEM:      clientKeyPEM,
+	}
+
+	access, err := uplink.ParseAccess(testAccess)
+	require.NoError(b, err)
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		p, err := config.OpenProject(ctx, access)
+		require.NoError(b, err)
+		require.NoError(b, p.Close())
+	}
+}
+
+func loadPEMs(b *testing.B, certPath, keyPath string) (certPEM, keyPEM []byte) {
+	if certPath == "" || keyPath == "" {
+		b.Skipf("only one of key path and cert path are set (%q, %q)", keyPath, certPath)
+	}
+
+	certPEM, err := os.ReadFile(certPath)
+	require.NoError(b, err)
+
+	keyPEM, err = os.ReadFile(keyPath)
+	require.NoError(b, err)
+
+	return certPEM, keyPEM
 }
