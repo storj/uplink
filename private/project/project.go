3179c8d1ccdb9e62b43c5ec63a8b5b2094490984
Wilfred Asomani <devwilfredagyei@gmail.com>
2025-01-23 12:26:57 +0000

    private/{project,testsuite}: add GetProjectInfo method
    This new method calls the endpoint of the same name on the satellite.
    It contains salt, public ID, and edge URL overrides if any.
    
    Related: storj/storj#6189
    
    Change-Id: I42652eeaea70ddc0b78b0b8bb6166989454aa0ed

--- private/project/project.go
+++ private/project/project.go
@@ -11,6 +11,7 @@ import (
 	"github.com/spacemonkeygo/monkit/v3"
 	"github.com/zeebo/errs"
 
+	"storj.io/common/pb"
 	"storj.io/common/signing"
 	"storj.io/common/uuid"
 	"storj.io/uplink"
@@ -31,29 +32,75 @@ var (
 	ErrLockNotEnabled = errors.New("object lock is not enabled")
 )
 
-// GetPublicID gets the public project ID for the given access grant.
-func GetPublicID(ctx context.Context, config uplink.Config, access *uplink.Access) (id uuid.UUID, err error) {
-	defer mon.Task()(&ctx)(&err)
+// EdgeURLOverrides contains the URL overrides for the edge service.
+type EdgeURLOverrides struct {
+	AuthService         string
+	PublicLinksharing   string
+	InternalLinksharing string
+}
 
+// Info contains the public ID, salt and edge url overrides of a project.
+type Info struct {
+	PublicId         uuid.UUID
+	Salt             []byte
+	EdgeUrlOverrides *EdgeURLOverrides
+}
+
+func getProjectInfo(ctx context.Context, config uplink.Config, access *uplink.Access) (_ *pb.ProjectInfoResponse, err error) {
 	dialer, err := expose.ConfigGetDialer(config, ctx)
 	if err != nil {
-		return id, packageError.Wrap(err)
+		return nil, packageError.Wrap(err)
 	}
 	defer func() { err = errs.Combine(err, dialer.Pool.Close()) }()
 
 	client, err := metaclient.DialNodeURL(ctx, dialer, access.SatelliteAddress(), expose.AccessGetAPIKey(access), config.UserAgent)
 	if err != nil {
-		return id, packageError.Wrap(err)
+		return nil, packageError.Wrap(err)
 	}
 	defer func() { err = errs.Combine(err, client.Close()) }()
 
 	info, err := client.GetProjectInfo(ctx)
+	return info, err
+}
+
+// GetPublicID gets the public project ID for the given access grant.
+func GetPublicID(ctx context.Context, config uplink.Config, access *uplink.Access) (id uuid.UUID, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	info, err := getProjectInfo(ctx, config, access)
 	if err != nil {
 		return id, convertKnownErrors(err, "", "")
 	}
 	copy(id[:], info.ProjectPublicId)
 
 	return id, nil
+}
+
+// GetProjectInfo gets the project info for the given access grant.
+func GetProjectInfo(ctx context.Context, config uplink.Config, access *uplink.Access) (_ Info, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	info, err := getProjectInfo(ctx, config, access)
+	if err != nil {
+		return Info{}, convertKnownErrors(err, "", "")
+	}
+	var id uuid.UUID
+	copy(id[:], info.ProjectPublicId)
+
+	projectInfo := Info{
+		PublicId: id,
+		Salt:     info.ProjectSalt,
+	}
+
+	if info.EdgeUrlOverrides != nil {
+		projectInfo.EdgeUrlOverrides = &EdgeURLOverrides{
+			AuthService:         string(info.EdgeUrlOverrides.AuthService),
+			PublicLinksharing:   string(info.EdgeUrlOverrides.PublicLinksharing),
+			InternalLinksharing: string(info.EdgeUrlOverrides.PrivateLinksharing),
+		}
+	}
+
+	return projectInfo, nil
 }
 
 //go:linkname convertKnownErrors storj.io/uplink.convertKnownErrors
--- testsuite/private/project/project_test.go
+++ testsuite/private/project/project_test.go
@@ -4,13 +4,17 @@
 package project_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 
 	"storj.io/common/macaroon"
+	"storj.io/common/storj"
 	"storj.io/common/testcontext"
 	"storj.io/storj/private/testplanet"
+	"storj.io/storj/satellite"
 	"storj.io/storj/satellite/console"
 	"storj.io/uplink"
 	privateProject "storj.io/uplink/private/project"
@@ -54,5 +58,72 @@ func TestGetPublicID(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, project.PublicID, id)
+	})
+}
+
+func TestGetProjectInfo(t *testing.T) {
+	var (
+		authUrl             = "auth.storj.io"
+		publicLinksharing   = "public-link.storj.io"
+		internalLinksharing = "link.storj.io"
+	)
+
+	testplanet.Run(t, testplanet.Config{
+		SatelliteCount: 1, UplinkCount: 1,
+		Reconfigure: testplanet.Reconfigure{
+			Satellite: func(log *zap.Logger, index int, config *satellite.Config) {
+				config.Metainfo.SendEdgeUrlOverrides = true
+				err := config.Console.PlacementEdgeURLOverrides.Set(
+					fmt.Sprintf(`{
+						"1": {
+							"authService": "%s",
+							"publicLinksharing": "%s",
+							"internalLinksharing": "%s"
+						}
+					}`, authUrl, publicLinksharing, internalLinksharing),
+				)
+				require.NoError(t, err)
+			},
+		},
+	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
+		sat := planet.Satellites[0]
+
+		user, err := sat.AddUser(ctx, console.CreateUser{
+			FullName: "test user", Email: "test-email@test", Password: "password",
+		}, 4)
+		require.NoError(t, err)
+
+		project, err := sat.AddProject(ctx, user.ID, "test project")
+		require.NoError(t, err)
+
+		secret, err := macaroon.NewSecret()
+		require.NoError(t, err)
+
+		apiKey, err := macaroon.NewAPIKey(secret)
+		require.NoError(t, err)
+
+		_, err = sat.DB.Console().APIKeys().Create(ctx, apiKey.Head(), console.APIKeyInfo{
+			Name: "testkey", ProjectID: project.ID, Secret: secret,
+		})
+		require.NoError(t, err)
+
+		access, err := uplink.RequestAccessWithPassphrase(ctx, sat.URL(), apiKey.Serialize(), "test")
+		require.NoError(t, err)
+
+		info, err := privateProject.GetProjectInfo(ctx, uplink.Config{}, access)
+		require.NoError(t, err)
+		require.NotNil(t, info.Salt)
+		require.Equal(t, project.PublicID, info.PublicId)
+		require.Nil(t, info.EdgeUrlOverrides)
+
+		err = sat.API.DB.Console().Projects().UpdateDefaultPlacement(ctx, project.ID, storj.PlacementConstraint(1))
+		require.NoError(t, err)
+
+		info, err = privateProject.GetProjectInfo(ctx, uplink.Config{}, access)
+		require.NoError(t, err)
+		require.NotNil(t, info.EdgeUrlOverrides)
+		require.Equal(t, authUrl, info.EdgeUrlOverrides.AuthService)
+		require.Equal(t, publicLinksharing, info.EdgeUrlOverrides.PublicLinksharing)
+		require.Equal(t, internalLinksharing, info.EdgeUrlOverrides.InternalLinksharing)
 	})
 }
